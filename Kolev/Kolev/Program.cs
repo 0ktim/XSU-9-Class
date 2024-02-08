74d551635626f0d5465c9674eb5d493b2d7e08ad
Venci kolev <vencimkolev@gmail.com>
2024-02-08 16:14:23 +0200

    for

--- Kolev/Kolev/Program.cs
+++ Kolev/Kolev/Program.cs
@@ -1,5 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Kolev");
 Console.WriteLine("Github");
+for (int i = 0; i < 10; i++) 
+{ 
+    Console.WriteLine(i); 
+}
 
 
