53a754ed4890c8d719a8b84a752f151a76ba81ea
unknown <d.angelowww.777@gmail.com>
2024-02-08 16:16:32 +0200

    add for

--- dimitar angelov/dimitar angelov/Program.cs
+++ dimitar angelov/dimitar angelov/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, dimitar angelov!");
 Console.WriteLine("github");
+for(int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+}
