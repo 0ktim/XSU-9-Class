ebea2c398be48229c2ddbb6f790f86af19e77784
Veselin Sv <sveselin3@gmail.com>
2024-02-08 16:13:53 +0200

    add for

--- Veselin-Stoyanov/Veselin-Stoyanov/Program.cs
+++ Veselin-Stoyanov/Veselin-Stoyanov/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Vesko!");
 Console.WriteLine("GitHub");
+
+for (int i = 0; i <= 10; i++)
+{
+    Console.WriteLine(i);
+}
