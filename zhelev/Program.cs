24f96ed13cb58df0b7df73a7271a2326546231c8
0ktim <oktim.v@gmail.com>
2024-02-08 16:13:54 +0200

    add for

--- zhelev/Program.cs
+++ zhelev/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Желев!");
 Console.WriteLine("GitHub");
+
+for (int i = 0; i <= 10; i++)
+{
+    Console.WriteLine(i);
+}
