74480c8a90f85c46e1103ba9862ca03b8f6fd4df
Nasko-5 <sect0r.mainacc@gmail.com>
2024-03-22 10:23:35 +0200

    add

---
+++ projects/searchinarray/Program.cs
@@ -0,0 +1,22 @@
+﻿while (true)
+{
+
+    string seprator = new string('-', Console.WindowWidth);
+    int[] array = new int[Random.Shared.Next(1, 100)].Select(a => Random.Shared.Next(-100, 100)).ToArray();
+    int toSearch = Random.Shared.Next(-100, 100);
+    bool found = false;
+    Console.WriteLine($"{seprator}{Environment.NewLine}Searching for {toSearch} in array[{string.Join(", ", array)}]{Environment.NewLine}{seprator}");
+    foreach (int i in array)
+    {
+        if (i == toSearch)
+        {
+            Console.WriteLine($"Item found!{Environment.NewLine}{seprator}");
+            found = true;
+            break;
+        }
+    }
+    if (!found) Console.WriteLine($"Item not found!{Environment.NewLine}{seprator}");
+
+    Thread.Sleep(100);
+    Console.Clear();
+}
