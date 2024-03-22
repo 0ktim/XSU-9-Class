3f167d524289616549fefdae80f91319dafd24c6
Mitko-1326 <d.angelowww.777@gmail.com>
2024-03-22 10:47:23 +0200

    нова задача

---
+++ array/ex.10/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] num = { 1, 2, 3, 4, 5, 6, 7, 8, 9, };
+
+foreach (int i in num)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine();
+Console.WriteLine("Въведете число:");
+
+int number = int.Parse(Console.ReadLine());
+
+bool check = false;
+foreach (int n in num)
+{
+    if (n== number)
+    {
+        check = true;
+    }
+}
+
+    if (check)
+    {
+        Console.WriteLine("Има");
+    }
+
+    else 
+    {
+        Console.WriteLine("Няма");
+    }
