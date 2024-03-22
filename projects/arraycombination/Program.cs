bbe5c18b5b6cc35e458f2c1c9853676424ad7bf8
Nasko-5 <sect0r.mainacc@gmail.com>
2024-03-22 09:37:47 +0200

    upload a thing

---
+++ projects/arraycombination/Program.cs
@@ -0,0 +1,24 @@
+﻿string seprator = new string('-', Console.WindowWidth);
+
+int[] array1 = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(int.Parse).ToArray();
+int[] array2 = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(int.Parse).ToArray();
+//int[] array3 = array1.Concat(array2).ToArray();
+
+int[] array3 = new int[array1.Length + array2.Length];
+
+
+
+for (int i = 0; i < array1.Length; i++)
+{
+    array3[i] = array1[i];
+}
+
+for (int i = 0; i < array2.Length; i++)
+{
+    array3[i + array1.Length] = array2[i];
+}
+
+Console.WriteLine($"{seprator}{Environment.NewLine}" +
+                  $"[{string.Join(", ", array1)}]{Environment.NewLine}{Environment.NewLine}" +
+                  $"[{string.Join(", ", array2)}]{Environment.NewLine}{seprator}{Environment.NewLine}" +
+                  $"[{string.Join(", ", array3)}]{Environment.NewLine}{seprator}");
