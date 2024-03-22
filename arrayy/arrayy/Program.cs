a748e626c486e6f2bac59dcf67699a455272c668
Veselin Stoyanov <159260934+VeselinSv@users.noreply.github.com>
2024-03-22 11:26:27 +0200

    add

---
+++ arrayy/arrayy/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace arrayEx1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            Console.WriteLine();
+            foreach (int num in nums)
+            {
+                Console.Write($"{num} ");
+            }
+            Console.WriteLine();
+            Console.Write("Има ли в масива този елемент: ");
+            int number = int.Parse(Console.ReadLine());
+            bool check = false;
+            foreach (int num in nums)
+            {
+                if (num == number)
+                {
+                    check = true;
+                }
+            }
+            Console.WriteLine();
+            if (check)
+            {
+                Console.WriteLine("Има");
+            }
+            else
+            {
+                Console.WriteLine("Няма");
+            }
+
+
+
+
+
+        }
+    }
+}
