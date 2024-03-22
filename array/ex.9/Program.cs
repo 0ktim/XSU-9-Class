21ee713ac12b9eb3e7777ffc44176e5609fa1cc7
Mitko-1326 <d.angelowww.777@gmail.com>
2024-03-22 09:02:51 +0200

    zadacha

---
+++ array/ex.9/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] num = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+int[] nums = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+int[] number = new int[num.Length + nums.Length];
+for (int i = 0; i < num.Length; i++)
+{
+    number[i] = num[i];
+}
+for (int j = 0; j < nums.Length; j++)
+{
+    number[num.Length + j] = nums[j];
+}
+foreach (int n in number)
+{
+    Console.WriteLine(n);
+}
