e8284cc39b1634cbbd557efe5bba2ce74f6e0828
Arco-IT <kaloyan.biser@gmail.com>
2024-03-22 10:39:10 +0200

    programa

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿while (true)
+{
+    int[] da1 = Console.ReadLine().Split(",").Select(int.Parse).ToArray();
+    int[] da2 = Console.ReadLine().Split(",").Select(int.Parse).ToArray();
+    Console.Clear();
+    int[] da3 = new int[da1.Length + da2.Length];
+    for (int i = 0; i < da1.Length; i++)
+    {
+        da3[i] = da1[i];
+    }
+    for (int j = 0; j < da2.Length; j++)
+    {
+        da3[da1.Length + j] = da2[j];
+    }
+    foreach (var asd in da3)
+    {
+        Console.Write(asd);
+    }
+}
