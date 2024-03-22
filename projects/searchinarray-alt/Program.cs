57cc8a7cfd01b40ee2dd7aaa622470643a00fcfe
Nasko-5 <sect0r.mainacc@gmail.com>
2024-03-22 10:28:16 +0200

    add

---
+++ projects/searchinarray-alt/Program.cs
@@ -0,0 +1,18 @@
+﻿string seprator = new string('-', Console.WindowWidth);
+int[] array = new int[Random.Shared.Next(1, 100)].Select(a => Random.Shared.Next(-100, 100)).ToArray();
+
+Console.Write($"{seprator}{Environment.NewLine}in array[{string.Join(", ", array)}]{Environment.NewLine}Search for: ");
+int toSearch = int.Parse(Console.ReadLine());
+bool found = false;
+Console.WriteLine($"{seprator}");
+
+foreach (int i in array)
+{
+    if (i == toSearch)
+    {
+        Console.WriteLine($"Item found!{Environment.NewLine}{seprator}");
+        found = true;
+        break;
+    }
+}
+if (!found) Console.WriteLine($"Item not found!{Environment.NewLine}{seprator}");
