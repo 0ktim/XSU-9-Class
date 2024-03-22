244eecea12e3fbf95195e2814af8d62ff283a122
ventsislav67 <vencimkolev@gmail.com>
2024-03-22 09:25:16 +0200

    update

---
+++ Domashna/masiv/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace masiv;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ masiv/masiv/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+int[] one = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+int[] two = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+int[] three = new int [one.Length + two.Length];
+for (int i = 0; i < one.Length; i++)
+{
+    three[i] = one[i];
+}
+for (int j = 0; j < two.Length; j++)
+{
+    three[one.Length + j  ] = two[j];
+}
+foreach (var num in three)
+{
+    Console.Write(num + ", ");
+}
