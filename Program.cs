ccf76d023cf8f6c42e3a94857efaad1197d2863a
Meto <159423833+M3TO777@users.noreply.github.com>
2024-03-22 09:18:46 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,4 +1,20 @@
-﻿for (int i = 1; i < 10; i++)
+﻿
+
+int[] array = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+int[] array2 = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+int[] pesho = new int[array.Length + array2.Length];
+for (int i = 0; i < array.Length; i++)
 {
-    Console.WriteLine( i);
+    pesho[i] = array[i];
 }
+for (int j = 0; j < array2.Length; j++)
+{
+    pesho[array.Length + j] = array2[j];
+}
+foreach(int i in pesho)
+{
+    Console.WriteLine(i);
+}
+
+
+
