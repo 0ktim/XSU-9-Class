b07681889dec065847798780dcb367959a142824
ventsislav67 <vencimkolev@gmail.com>
2024-03-22 09:29:10 +0200

    gotov e masiva

--- Domashna/masiv/Program.cs
+++ Domashna/masiv/Program.cs
@@ -3,6 +3,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[] one = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int[] two = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        int[] three = new int[one.Length + two.Length];
+        for (int i = 0; i < one.Length; i++)
+        {
+            three[i] = one[i];
+        }
+        for (int j = 0; j < two.Length; j++)
+        {
+            three[one.Length + j] = two[j];
+        }
+        foreach (var num in three)
+        {
+            Console.Write(num + ",");
+
+        }
     }
 }
--- Kolev/masivi/masivi/Program.cs
+++ Kolev/masivi/masivi/Program.cs
@@ -1,19 +1,18 @@
-﻿
-
+﻿// See https://aka.ms/new-console-template for more information
 int[] one = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
 int[] two = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
 
-int[] three = new int [one.Length + two.Length];
+int[] three = new int[one.Length + two.Length];
 for (int i = 0; i < one.Length; i++)
 {
     three[i] = one[i];
 }
 for (int j = 0; j < two.Length; j++)
 {
-    three[one.Length + j  ] = two[j];
+    three[one.Length + j] = two[j];
 }
 foreach (var num in three)
 {
     Console.Write(num + ",");
-    
 }
+
