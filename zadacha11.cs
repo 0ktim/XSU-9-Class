71cdb64fe8cc76435c8d0773b71ea603c8014b68
denica08 <denitsa88pa@gmail.com>
2024-03-22 11:09:45 +0200

    zadacha11

---
+++ zadacha11.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] nums = { 2, 4, 6, 8, };
+foreach (var item in nums)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine("Има ли в масиво числото:");
+int number = int.Parse(Console.ReadLine());
+foreach (var item in nums)
+{
+    if (number == item)
+    {
+        Console.WriteLine("True");
+    }
+    else if (number == item)
+    {
+        Console.WriteLine("False");
+    }
+}
