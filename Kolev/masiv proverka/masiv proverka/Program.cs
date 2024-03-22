eef5c589da9cf52b5c1b4a14b0e8d7e96c7fea56
ventsislav67 <vencimkolev@gmail.com>
2024-03-22 10:13:54 +0200

    masiv proverka

--- Kolev/masiv proverka/masiv proverka/Program.cs
+++ Kolev/masiv proverka/masiv proverka/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] n1 = { 1, 2, 3, 4, 5 };
+int[] n = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+int sum = 0;
+for (int i = 0; i < n.Length; i++)
+{
+    sum += n[i];
+    if (sum == n1[i])
+    {
+        Console.WriteLine("yes"); break;
+    }
+    else { Console.WriteLine("no"); break; }
+}
