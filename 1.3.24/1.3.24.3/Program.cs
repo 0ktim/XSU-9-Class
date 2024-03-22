226252638388c46265878feec470c9c32f1ce88a
Veselin Stoyanov <159260934+VeselinSv@users.noreply.github.com>
2024-03-22 10:18:08 +0200

    Delete 1.3.24 directory

--- 1.3.24/1.3.24.3/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿
-string numberHex = Console.ReadLine();
-int number10 = Convert.ToInt32(numberHex, 16);
-Console.WriteLine(number10);
--- 1.3.24/1.3.24/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Threading.Channels;
-
-int centuries = int.Parse(Console.ReadLine());
-int years = centuries * 100;
-int days = (int)(years * 365.2422);
-int hours = days * 24;
-int minutes = hours * 60;
-Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
