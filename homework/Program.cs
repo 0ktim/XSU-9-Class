aebbfeddf88083307c2fe921183567bf44c508ab
Gotika111 <159442236+Gotika111@users.noreply.github.com>
2024-03-22 09:33:36 +0200

    array homework

---
+++ homework/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Въведете два масива с ' ' :");
+            int[] ints = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int[] ints2 = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int[] ints3 = new int[ints.Length + ints2.Length];
+
+            for (int i = 0; i < ints.Length; i++)
+            {
+                ints3[i] = ints[i];
+            }
+
+            for (int j = 0; j < ints2.Length; j++)
+            {
+                ints3[ints.Length + j] = ints2[j];
+            }
+
+            foreach (var int3 in ints3)
+            {
+                Console.Write(int3 + " ");
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
+
