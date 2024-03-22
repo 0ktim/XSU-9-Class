d555505f85647efcea4761d2c6b777c175a25d9e
ventsislav67 <vencimkolev@gmail.com>
2024-03-22 10:28:03 +0200

    i 5 raboti

--- Kolev/masiv proverka/masiv proverka/Program.cs
+++ Kolev/masiv proverka/masiv proverka/Program.cs
@@ -1,12 +1,18 @@
-﻿int[] n1 = { 1, 2, 3, 4, 5 };
-int[] n = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
-int sum = 0;
-for (int i = 0; i < n.Length; i++)
+﻿while (true)
 {
-     int t = sum + n1[i];
-    if (n[i] < n1.Length)
+
+
+    int[] n1 = { 1, 2, 3, 4, 5 };
+
+    int[] n = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+    int sum = 0;
+    for (int i = 0; i < n.Length; i++)
     {
-        Console.WriteLine("yes"); break;
+        int t = sum + n1[i];
+        if (n[i] <= n1.Length)
+        {
+            Console.WriteLine("yes"); break;
+        }
+        else if (n[i] > n1.Length) { Console.WriteLine("no"); break; }
     }
-    else if (n[i] > n1.Length) { Console.WriteLine("no"); break; }
 }
