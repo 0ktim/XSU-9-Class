cf2d1300ea8fa409e39de369b55bfe5762b4971c
ventsislav67 <vencimkolev@gmail.com>
2024-03-31 01:36:44 +0200

    changes

---
+++ Domashna/masiv3/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace masiv3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums1 = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int[] nums2 = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            Console.Clear();
+            int[] nums3 = new int[nums1.Length + nums2.Length];
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                nums3[i] = nums1[i];
+            }
+            for (int j = 0; j < nums2.Length; j++)
+            {
+                nums3[nums1.Length + j] = nums2[j];
+            }
+            foreach (int num3 in nums3)
+            {
+                Console.WriteLine(num3);
+            }
+    }
