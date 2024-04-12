b325cb00f7254f932d96918841d5491383b09edc
VeselinSv <sveselin3@gmail.com>
2024-04-12 14:46:42 +0300

    homework

---
+++ class1/circle/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace circle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            classCircle circle = new classCircle();
+            circle.Radius = double.Parse(Console.ReadLine());
+            circle.AllInOne();
+
+        }
+    }
+}
---
+++ class1/circle/classCircle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace circle
+{
+    internal class classCircle
+    {
+        public double Radius;
+
+        public double KalculateArea() 
+        {
+            return Math.PI * Radius * Radius;
+        }
+        public double KalculatePerimeter() 
+        {
+            return 2 * Math.PI * Radius;
+        }
+        public void AllInOne() 
+        {
+            Console.WriteLine($"Площа е: {KalculateArea()} и периметъра е {KalculatePerimeter()}.");
+        }
+    }
+}
