c447ac29ebd4cb4dc8098244334600bd8bfbaa29
VeselinSv <sveselin3@gmail.com>
2024-04-12 14:21:25 +0300

    homework

---
+++ class1/class1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace class1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            human people = new human();
+            Console.Write("Името Ви е: ");
+            people.Name = Console.ReadLine();
+            people.Age = int.Parse(Console.ReadLine());
+            people.Sex = Console.ReadLine();
+            people.AllInOne();
+        }
+    }
+}
---
+++ class1/class1/human.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class1
+{
+    internal class human
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string Sex { get; set; }
+
+        public void AllInOne()
+        {
+            Console.WriteLine($"Име: {Name}. Години: {Age}. Пол: {Sex}");
+        }
+    }
+}
