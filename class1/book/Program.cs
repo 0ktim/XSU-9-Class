1eb344bfa5cd31ec8a933539616d0d0a7057f515
VeselinSv <sveselin3@gmail.com>
2024-04-12 14:36:37 +0300

    homework

--- class1/book/Program.cs
+++ class1/book/Program.cs
@@ -5,9 +5,13 @@
         static void Main(string[] args)
         {
             classBook book = new classBook();
+            Console.Write("Заглавието на книгата е: ");
             book.title = Console.ReadLine();
+            Console.Write("Автор на книгата е: ");
             book.author = Console.ReadLine();
+            Console.Write("Година на издаване на книгата е: ");
             book.year = int.Parse(Console.ReadLine());
+            book.AllInOne();
 
         }
     }
