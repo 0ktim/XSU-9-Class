e5506f48219ca53bfbc67e49d38164a45cdcd1ec
ventsislav67 <vencimkolev@gmail.com>
2024-04-14 12:00:28 +0300

    task 2!

---
+++ Domashna/class/Book/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book
+{
+    internal class Book
+    {
+        public string title;
+        public string author;
+        public int lunch;
+        public void allforbook() {
+            Console.WriteLine($" Книга със заглавие {title} и автор {author} e издадена през {lunch} година.   ");
+        }
+    }
+}
---
+++ Domashna/class/Book/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Book
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            Console.Write("Въведете заглавие:");
+            book.title = Console.ReadLine();
+            Console.Write("Въведете автор:");
+            book.author = Console.ReadLine();
+            Console.Write("Въведете година на издаване:");
+            book.lunch = int.Parse(Console.ReadLine());
+            book.allforbook();
+        }
+    }
+}
