4dc8047ca9b65bbf41443bc9d5bc0059c10f247d
ventsislav67 <vencimkolev@gmail.com>
2024-04-14 13:02:11 +0300

    task 4!

---
+++ Domashna/class/specialist/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.Design;
+
+namespace specialist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            specialist specialist = new specialist();
+            Console.Write("Въведете име на служител:");
+            specialist.name = Console.ReadLine();
+            Console.Write("Въведете отдел:");
+            specialist.otdel = Console.ReadLine();
+            Console.Write("Въведете заплата:");
+            specialist.Salary = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Искате ли да промените заплата или отдел?");
+            string change = Console.ReadLine();
+            if (change == "да")
+            {
+                Console.WriteLine("Отдел или заплата?");
+                string or = Console.ReadLine();
+                if (or == "отдел")
+                {
+                    specialist.changeotdel();
+                }
+                else  
+                {
+                    specialist.changesalary();
+                }
+
+            }
+            else { specialist.allinformation(); }
+           
+
+        }
+        
+        }
+    }
---
+++ Domashna/class/specialist/specialist.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace specialist
+{
+    internal class specialist
+    {
+        public string name { get; set; }
+        public int Salary { get; set; }
+        public string otdel { get; set; }
+
+        public void changeotdel(  ) {
+            Console.WriteLine("Въведете новия отдел:");
+            string newotdel = Console.ReadLine();
+            string novotdel = newotdel;    
+            Console.WriteLine($"{name} от отдел {novotdel} със заплата {Salary}.");
+
+
+        }
+
+        public void allinformation() {
+            Console.WriteLine($"{name} от отдел {otdel} със заплата {Salary}.");
+        }
+
+        public void changesalary() {
+            Console.WriteLine("Въведете нова заплата:");
+            string newsalary = Console.ReadLine();
+        string novtsalary = newsalary;
+            Console.WriteLine($"{name} от отдел {otdel} със заплата {novtsalary}.");
+
+        }
+
+
+    }
+}
