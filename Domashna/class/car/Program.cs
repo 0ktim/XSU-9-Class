45868035846bc51b1699b2606669997ab288e11c
ventsislav67 <vencimkolev@gmail.com>
2024-04-14 13:10:48 +0300

    task 5!

---
+++ Domashna/class/car/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace car
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            car car = new car();
+            Console.Write("Въведете марка на автомобила:");
+            car.brand = Console.ReadLine();
+            Console.Write("Въведете модел на автомобила:");
+            car.model = Console.ReadLine();
+            Console.Write("Въведете година на производство: ");
+            car.lunch = int.Parse(Console.ReadLine());
+            car.allinformationforcar();
+        }
+    }
+}
---
+++ Domashna/class/car/car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace car
+{
+    internal class car
+    {
+        public string brand;
+        public string model;
+        public int lunch;
+
+        public void allinformationforcar() {
+            Console.WriteLine($"Колата {brand} {model} е произведена през {lunch}.");
+
+        }
+    }
+}
