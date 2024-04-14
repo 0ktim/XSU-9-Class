71b56f4348fc2fa5f7e4093b2bf82cca26a3bcb3
ventsislav67 <vencimkolev@gmail.com>
2024-04-14 12:16:34 +0300

    task 3!

---
+++ Domashna/class/circle/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace circle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            circle circle = new circle();
+            Console.Write("Въведете радиуса на кръга:");
+            circle.Radius = double.Parse(Console.ReadLine());
+            Console.WriteLine("Лицето на кръга е:");
+            Console.WriteLine(circle.perimeter());
+            Console.WriteLine("Обиколката на кръга е:");
+            Console.WriteLine(circle.area()); 
+
+        }
+    }
+}
---
+++ Domashna/class/circle/circle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace circle
+{
+    internal class circle
+    {
+        public double Radius { get; set; }
+        public double perimeter() {
+            return 2 * Radius * Radius;
+        }
+        public double area() {
+        return Math.PI * Radius * Radius;
+        }
+    }
+}
