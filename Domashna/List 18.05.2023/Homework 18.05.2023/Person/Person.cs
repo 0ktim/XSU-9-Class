ffea04412967f7fd30c3ec5298f28e3115785882
ventsislav67 <vencimkolev@gmail.com>
2024-05-19 14:14:58 +0300

    Add task 1!

---
+++ Domashna/List 18.05.2023/Homework 18.05.2023/Person/Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+       
+        
+       
+
+        
+
+       
+
+    public Person (string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+            filter();
+
+            void filter () {
+                foreach (var person in gender)
+                {
+                    if (gender == "Male")
+                    {
+                        if (age == 18)
+                        {
+                            Console.WriteLine(name);break;
+
+                        }
+                    }
+                }
+
+            }
+
+        }
+
+
+
+
+        
+    }
+}
+
---
+++ Domashna/List 18.05.2023/Homework 18.05.2023/Person/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+
+namespace Person
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            List<Person> people = new List<Person>();
+            people.Add(new Person("Ivan", 35, "Male"));
+            people.Add(new Person("Gosho", 18, "Male"));
+            people.Add(new Person("Maria", 18, "Female"));
+            people.Add(new Person("Jivka", 25, "Female"));
+            people.Add(new Person("Venci", 19, "Male"));
+            people.Add(new Person("Dragan", 18, "Male"));
+            
+
+
+
+           
+        }
+    }
+}
