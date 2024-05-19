0dec5c6527d4f237b7678ed79cc953842c6cd24e
ventsislav67 <vencimkolev@gmail.com>
2024-05-19 14:37:54 +0300

    Add task 2!

---
+++ Domashna/List 18.05.2023/Homework 18.05.2023/Movie/Movie.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movie
+{
+    internal class Movie
+    {
+        public string Title { get; set; }
+        public string director { get; set; }
+        public int dateoflunch { get; set; }
+
+        public Movie(string Title, string director, int dateoflunch)
+        {
+            this.Title = Title;
+            this.director = director;
+            this.dateoflunch = dateoflunch;
+            agefilter();
+            void agefilter() 
+            {
+                while (dateoflunch > 2010) {
+                    Console.WriteLine(Title);break;
+
+
+                }
+
+            }
+
+        }
+    }
+}
---
+++ Domashna/List 18.05.2023/Homework 18.05.2023/Movie/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Movie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Movie> list = new List<Movie>();
+            list.Add(new Movie("Fast and Furison 1", "Роб Коен", 2001));
+            list.Add(new Movie("Fast and Furison 2","Джон Сингълтън",2003));
+            list.Add(new Movie("Fast and Furison 3", "Джъстин Лин", 2006));
+            list.Add(new Movie("Fast and Furison 4", "Джъстин Лин", 2009));
+            list.Add(new Movie("Fast and Furison 5", "Джъстин Лин", 2011));
+            list.Add(new Movie("Fast and Furison 6", "Джъстин Лин", 2013));
+            list.Add(new Movie("Fast and Furison 7", "Джеймс Уан", 2015));
+            list.Add(new Movie("Fast and Furison 8", "Феликс Гари Грей", 2017));
+            list.Add(new Movie("Fast and Furison 9", "Джъстин Лин", 2021));
+            list.Add(new Movie("Fast and Furison 10", "Луи Льотерие", 2023));
+
+
+        }
+    }
+}
