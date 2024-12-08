4a96573a1db8a622fec1cc147eb882da7bc9788c
Veselin Stoyanov <159260934+VeselinSv@users.noreply.github.com>
2024-12-08 13:29:54 +0200

    final add

---
+++ MaxNumber/Form1.cs
@@ -0,0 +1,40 @@
+﻿namespace MaxNumber
+{
+    public partial class Form1 : Form
+    {
+        private number Max;
+        public Form1()
+        {
+            InitializeComponent();
+            Max = new number();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try 
+            {
+            int numBox1 = int.Parse(textBox1.Text);
+            int numBox2 = int.Parse(textBox2.Text);
+
+            int MaxNum = Max.findMax(numBox1, numBox2);
+
+            label3.Text = MaxNum.ToString();
+            } 
+            
+            catch 
+            {
+                MessageBox.Show("Невалидни данни!", "Грешка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MaxNumber/number.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxNumber
+{
+    internal class number
+    {
+        public int findMax(int numBox1 , int numBox2) 
+        {
+        if (numBox1 > numBox2)
+            {
+                return numBox1;
+            }
+        else 
+            {
+                return numBox2;
+            }
+
+        }
+    }
+}
