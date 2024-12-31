da8399291af4a2025be93ce7d626a09d04504aac
ventsislav67 <vencimkolev@gmail.com>
2024-12-31 12:34:51 +0200

    Library///christmas homework

---
+++ Domashna/Library/Library/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Library
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            btndelete = new Button();
+            btnFindBook = new Button();
+            btnAddBook = new Button();
+            tbTitle = new TextBox();
+            btnSave = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            tbAuthor = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
+            SuspendLayout();
+            // 
+            // btndelete
+            // 
+            btndelete.BackColor = Color.White;
+            btndelete.BackgroundImage = (Image)resources.GetObject("btndelete.BackgroundImage");
+            btndelete.BackgroundImageLayout = ImageLayout.Stretch;
+            btndelete.FlatAppearance.BorderSize = 0;
+            btndelete.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            btndelete.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            btndelete.FlatStyle = FlatStyle.Popup;
+            btndelete.Location = new Point(105, 335);
+            btndelete.Name = "btndelete";
+            btndelete.Size = new Size(94, 36);
+            btndelete.TabIndex = 0;
+            btndelete.UseVisualStyleBackColor = false;
+            btndelete.Click += btndelete_Click_1;
+            // 
+            // btnFindBook
+            // 
+            btnFindBook.BackColor = Color.White;
+            btnFindBook.BackgroundImage = (Image)resources.GetObject("btnFindBook.BackgroundImage");
+            btnFindBook.BackgroundImageLayout = ImageLayout.Stretch;
+            btnFindBook.FlatAppearance.BorderSize = 0;
+            btnFindBook.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            btnFindBook.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            btnFindBook.FlatStyle = FlatStyle.Popup;
+            btnFindBook.ForeColor = Color.White;
+            btnFindBook.Location = new Point(105, 273);
+            btnFindBook.Name = "btnFindBook";
+            btnFindBook.Size = new Size(94, 41);
+            btnFindBook.TabIndex = 1;
+            btnFindBook.UseVisualStyleBackColor = false;
+            btnFindBook.Click += btnFindBook_Click_1;
+            // 
+            // btnAddBook
+            // 
+            btnAddBook.BackColor = Color.White;
+            btnAddBook.BackgroundImage = (Image)resources.GetObject("btnAddBook.BackgroundImage");
+            btnAddBook.BackgroundImageLayout = ImageLayout.Stretch;
+            btnAddBook.FlatAppearance.BorderSize = 0;
+            btnAddBook.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            btnAddBook.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            btnAddBook.FlatStyle = FlatStyle.Flat;
+            btnAddBook.ForeColor = Color.White;
+            btnAddBook.Location = new Point(95, 79);
+            btnAddBook.Name = "btnAddBook";
+            btnAddBook.Size = new Size(104, 44);
+            btnAddBook.TabIndex = 2;
+            btnAddBook.UseVisualStyleBackColor = false;
+            btnAddBook.Click += btnAddBook_Click_1;
+            // 
+            // tbTitle
+            // 
+            tbTitle.Location = new Point(12, 228);
+            tbTitle.Name = "tbTitle";
+            tbTitle.Size = new Size(125, 27);
+            tbTitle.TabIndex = 4;
+            // 
+            // btnSave
+            // 
+            btnSave.BackColor = Color.White;
+            btnSave.BackgroundImage = (Image)resources.GetObject("btnSave.BackgroundImage");
+            btnSave.BackgroundImageLayout = ImageLayout.Stretch;
+            btnSave.FlatAppearance.BorderSize = 0;
+            btnSave.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            btnSave.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            btnSave.FlatStyle = FlatStyle.Popup;
+            btnSave.ForeColor = Color.White;
+            btnSave.Location = new Point(105, 395);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(94, 42);
+            btnSave.TabIndex = 5;
+            btnSave.UseVisualStyleBackColor = false;
+            btnSave.Click += btnSave_Click_1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Showcard Gothic", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.Control;
+            label1.Location = new Point(41, 35);
+            label1.Name = "label1";
+            label1.Size = new Size(201, 28);
+            label1.TabIndex = 6;
+            label1.Text = "Добавяне на книга";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.PeachPuff;
+            label2.BorderStyle = BorderStyle.FixedSingle;
+            label2.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = SystemColors.ActiveCaptionText;
+            label2.Location = new Point(41, 188);
+            label2.Name = "label2";
+            label2.Size = new Size(83, 25);
+            label2.TabIndex = 7;
+            label2.Text = "Заглавие";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.PeachPuff;
+            label3.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(191, 190);
+            label3.Name = "label3";
+            label3.Size = new Size(57, 23);
+            label3.TabIndex = 8;
+            label3.Text = "Автор";
+            // 
+            // tbAuthor
+            // 
+            tbAuthor.Location = new Point(159, 228);
+            tbAuthor.Name = "tbAuthor";
+            tbAuthor.Size = new Size(125, 27);
+            tbAuthor.TabIndex = 9;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Showcard Gothic", 13.2000008F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.ForeColor = SystemColors.Control;
+            label4.Location = new Point(22, 135);
+            label4.Name = "label4";
+            label4.Size = new Size(238, 29);
+            label4.TabIndex = 10;
+            label4.Text = "Намиране на книга:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Showcard Gothic", 13.2000008F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.ForeColor = SystemColors.Control;
+            label5.Location = new Point(301, 34);
+            label5.Name = "label5";
+            label5.Size = new Size(149, 29);
+            label5.TabIndex = 11;
+            label5.Text = "Библиотека";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1033, 525);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(tbAuthor);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btnSave);
+            Controls.Add(tbTitle);
+            Controls.Add(btnAddBook);
+            Controls.Add(btnFindBook);
+            Controls.Add(btndelete);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btndelete;
+        private Button btnFindBook;
+        private Button btnAddBook;
+        private TextBox tbTitle;
+        private Button btnSave;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox tbAuthor;
+        private Label label4;
+        private Label label5;
+    }
+}
---
+++ Domashna/Library/Library/Form1.cs
@@ -0,0 +1,306 @@
+﻿using System.Data;
+using System.Text;
+
+namespace Library
+{
+
+    public partial class Form1 : Form
+    {
+        private DataGridView books;
+        private DataTable booksTable;
+        public Form1()
+        {
+            InitializeComponent();
+            BooksList();
+
+        }
+
+        private void BooksList()
+        {
+            books = new DataGridView();
+            books.Dock = DockStyle.Right;
+            books.Width = 500;
+            this.Controls.Add(books);
+
+            booksTable = new DataTable();
+
+            booksTable.Columns.Add("Книга");
+            booksTable.Columns.Add("Автор");
+
+            booksTable.Rows.Add("Под игото", "Иван Вазов");
+            booksTable.Rows.Add("Тютюн", "Димитър Димов");
+            booksTable.Rows.Add("Железният светилник", "Димитър Талев");
+            booksTable.Rows.Add("Осъдени души", "Димитър Димов");
+            booksTable.Rows.Add("Бай Ганьо", "Алеко Константинов");
+            booksTable.Rows.Add("Чичовци", "Иван Вазов");
+            booksTable.Rows.Add("Ангелинка", "Елин Пелин");
+            booksTable.Rows.Add("Гераците", "Елин Пелин");
+            booksTable.Rows.Add("Хайка за Вълци", "Ивайло Петлов");
+            booksTable.Rows.Add("Време Разделно", "Антон Дончев");
+            booksTable.Rows.Add("Приключенията на Лиско", "Борис Априлов");
+            booksTable.Rows.Add("Козият рог", "Николай Хайтов");
+            booksTable.Rows.Add("Диви разкази", "Николай Хайтов");
+            booksTable.Rows.Add("Милионерът", "Елин Пелин");
+            booksTable.Rows.Add("Трите живота на Кракра", "Антон Дончев");
+            booksTable.Rows.Add("Земя", "Елин Пелин");
+            booksTable.Rows.Add("На разсъмване", "Йордан Йовков");
+            booksTable.Rows.Add("Старопланински Легенди", "Йордан Йовков");
+            booksTable.Rows.Add("Албена", "Йордан Йовков");
+            booksTable.Rows.Add("Нощем с белите коси", "Павел Вежинов");
+            booksTable.Rows.Add("Калуня-Каня", "Георги Божинов");
+            booksTable.Rows.Add("Легенда за Сибин", "Евгени Кузманов");
+            booksTable.Rows.Add("Мъртви души", "Алеко Константинов");
+            booksTable.Rows.Add("Писма до Ганка", "Йордан Радичков");
+            booksTable.Rows.Add("Ние,врабчетата", "Йордан Радичков");
+            booksTable.Rows.Add("Лавина", "Блага Димитрова");
+            booksTable.Rows.Add("Петко войвода", "Николай Райнов");
+            booksTable.Rows.Add("Изумруденото око", "Людмила Филипова");
+            booksTable.Rows.Add("Шибил", "Йордан Йовков");
+            booksTable.Rows.Add("Бяла пръст", "Георги Караславов");
+            booksTable.Rows.Add("Цената на златото", "Генчо Стоев");
+            booksTable.Rows.Add("Почивка в Арко Ирис", "Георги Господинов");
+            booksTable.Rows.Add("Естествен роман", "Георги Господинов");
+            booksTable.Rows.Add("Физика на тъгата", "Георги Господинов");
+            booksTable.Rows.Add("Иван Кондарев", "Емилиян Станев");
+            booksTable.Rows.Add("Антихрист", "Емилиян Станев");
+            booksTable.Rows.Add("Крадецът на праскови", "Емилиян станев");
+            booksTable.Rows.Add("В полите на Витоша", "Пейо Яворов");
+            booksTable.Rows.Add("Моторни песни", "Гео Милев");
+            booksTable.Rows.Add("Писма от България", "Алеко Константинов");
+            booksTable.Rows.Add("Смъртта на Аякс", "Петър Увалиев");
+            booksTable.Rows.Add("Преспанските камбани", "Димитър Талев");
+            booksTable.Rows.Add("Татул", "Николай Хайтов");
+            booksTable.Rows.Add("Денят не си личи по заранта", "Николай Райнов");
+            booksTable.Rows.Add("Самуил", "Димитър Талев");
+            booksTable.Rows.Add("Пътешественикът", "Николай Кънчев");
+            booksTable.Rows.Add("Кръвта остава", "  Георги Краславов");
+            booksTable.Rows.Add("Потъването на Созопол", "Ина Вълчанова");
+
+
+
+            books.DataSource = booksTable;
+            books.ReadOnly = true;
+            books.AllowUserToAddRows = false;
+            books.AllowUserToDeleteRows = false;
+
+            books.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            books.ColumnHeadersDefaultCellStyle.Font = new System.Drawing.Font("Arial", 10, System.Drawing.FontStyle.Bold);
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            DataGridViewRow row = books.SelectedRows[0];
+
+            string bookName = row.Cells["Книга"].Value.ToString();
+            string authorName = row.Cells["Автор"].Value.ToString();
+
+            if (books.SelectedRows.Count > 0)
+            {
+                if (MessageBox.Show($"Книгата '{bookName}' на автора {authorName}?",
+                    "Question", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    books.Rows.Remove(row);
+                }
+            }
+        }
+
+        private void btnFindBook_Click(object sender, EventArgs e)
+        {
+            string searchText = tbTitle.Text.ToLower();
+            bool found = false;
+
+            foreach (DataGridViewRow row in books.Rows)
+            {
+                string bookTitle = row.Cells["Книга"].Value.ToString().ToLower();
+                string author = row.Cells["Автор"].Value.ToString().ToLower();
+
+                if (bookTitle.Contains(searchText) || author.Contains(searchText))
+                {
+                    row.Selected = true;
+                    found = true;
+                    tbTitle.Clear();
+                    tbAuthor.Clear();
+                }
+            }
+            if (!found)
+            {
+                MessageBox.Show("Намерена!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+
+        private void btnAddBook_Click(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(tbTitle.Text, "Заглавие") || System.Text.RegularExpressions.Regex.IsMatch(tbAuthor.Text, "Автор"))
+            {
+                MessageBox.Show("Вече е налично");
+
+            }
+            else
+            {
+                booksTable.Rows.Add(tbTitle.Text, tbAuthor.Text);
+                MessageBox.Show("Добавено усрешно!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SaveFileDialog savefd = new SaveFileDialog();
+                savefd.Filter = "CSV Files|*.csv";
+                savefd.FileName = "Запазено успешно";
+
+                if (savefd.ShowDialog() == DialogResult.OK)
+                {
+                    StringBuilder sb = new StringBuilder();
+
+                    List<string> columnHeaders = new List<string>();
+
+                    List<string> columnNames = new List<string>();
+                    foreach (DataGridViewColumn column in books.Columns)
+                    {
+                        columnHeaders.Add(column.HeaderText);
+                    }
+                    sb.AppendLine(string.Join(",", columnHeaders));
+
+                    foreach (DataGridViewRow row in books.Rows)
+                    {
+                        if (!row.IsNewRow)
+                        {
+                            List<string> rowText = new List<string>();
+
+                            foreach (DataGridViewCell cell in row.Cells)
+                            {
+                                string cellValue = cell.Value?.ToString() ?? "";
+                                rowText.Add(cellValue);
+                            }
+                            sb.AppendLine(string.Join(",", rowText));
+                        }
+                    }
+
+                    File.WriteAllText(savefd.FileName, sb.ToString(), new UTF8Encoding(true));
+
+                    MessageBox.Show("Записано успешно!", "Information",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Грешка: {ex.Message}", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnFindBook_Click_1(object sender, EventArgs e)
+        {
+            string searchText = tbTitle.Text.ToLower();
+            bool found = false;
+
+            foreach (DataGridViewRow row in books.Rows)
+            {
+                string bookTitle = row.Cells["Книга"].Value.ToString().ToLower();
+                string author = row.Cells["Автор"].Value.ToString().ToLower();
+
+                if (bookTitle.Contains(searchText) || author.Contains(searchText))
+                {
+                    row.Selected = true;
+                    found = true;
+                    tbTitle.Clear();
+                }
+            }
+            if (!found)
+            {
+                MessageBox.Show("Няма намерена книга или автор!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnSave_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                SaveFileDialog savefd = new SaveFileDialog();
+                savefd.Filter = "CSV Files|*.csv";
+                savefd.FileName = "Запазено успешно";
+
+                if (savefd.ShowDialog() == DialogResult.OK)
+                {
+                    StringBuilder sb = new StringBuilder();
+
+                    List<string> columnHeaders = new List<string>();
+
+                    List<string> columnNames = new List<string>();
+                    foreach (DataGridViewColumn column in books.Columns)
+                    {
+                        columnHeaders.Add(column.HeaderText);
+                    }
+                    sb.AppendLine(string.Join(",", columnHeaders));
+
+                    foreach (DataGridViewRow row in books.Rows)
+                    {
+                        if (!row.IsNewRow)
+                        {
+                            List<string> rowText = new List<string>();
+
+                            foreach (DataGridViewCell cell in row.Cells)
+                            {
+                                string cellValue = cell.Value?.ToString() ?? "";
+                                rowText.Add(cellValue);
+                            }
+                            sb.AppendLine(string.Join(",", rowText));
+                        }
+                    }
+
+                    File.WriteAllText(savefd.FileName, sb.ToString(), new UTF8Encoding(true));
+
+                    MessageBox.Show("Записано успешно!", "Information",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Грешка: {ex.Message}", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnAddBook_Click_1(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(tbTitle.Text, "Заглавие") || System.Text.RegularExpressions.Regex.IsMatch(tbAuthor.Text, "Автор"))
+            {
+                MessageBox.Show("Вече е налично");
+
+            }
+            else
+            {
+                booksTable.Rows.Add(tbTitle.Text, tbAuthor.Text);
+                MessageBox.Show("Добавено упрешно!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btndelete_Click_1(object sender, EventArgs e)
+        {
+            DataGridViewRow row = books.SelectedRows[0];
+
+            string bookName = row.Cells["Книга"].Value.ToString();
+            string authorName = row.Cells["Автор"].Value.ToString();
+
+            if (books.SelectedRows.Count > 0)
+            {
+                if (MessageBox.Show($"Книгата '{bookName}' на автора {authorName}?",
+                    "Question", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    books.Rows.Remove(row);
+                }
+            }
+        }
+    }
+}
